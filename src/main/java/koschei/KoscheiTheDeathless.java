46067b3811114875ab47c76a926a5e330e540cd0
Никита <mikhailovnikita1998@gmail.com>
2024-04-25 21:14:06 +0900

    1

--- src/main/java/koschei/KoscheiTheDeathless.java
+++ src/main/java/koschei/KoscheiTheDeathless.java
@@ -1,6 +1,7 @@
 package koschei;
 
-import koschei.models.Ocean1;
+import koschei.config.AppConfig;
+import koschei.models.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
